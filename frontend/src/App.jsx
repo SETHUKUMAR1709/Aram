c54325c0d764f0cb9ab2238111bc4ddbe1fd57b6
SETHUKUMAR1709 <sethukumars774@gmail.com>
2025-12-20 22:08:47 +0530

    bug - fix for deploayment

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,83 +1,64 @@
-import OnBoardingPage from "./pages/OnBoardingPage";
-import { ThemeProvider } from "./contexts/theme-provider";
-import ProtectedRoute from "./contexts/ProtectedRoute.jsx";
-import AuthRedirect from "./contexts/AuthRedirect";
-import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
-import LoginPage from "./pages/auth/LoginPage";
-import SignupPage from "./pages/auth/SignupPage";
-import ChatPage from "./pages/ChatPage";
-import { useAuthStore } from "./store/useAuthStore";
-import { useEffect } from "react";
-import { Loader } from "lucide-react";
-import Layout from "./contexts/LayoutContext";
-import RedirectToLatestChat from "./contexts/RedirectToLatestChat";
-import LawyerPage from "./pages/LawyerPage";
-import ContactPage from "./pages/ContactPage";
-
 function App() {
   const { isCheckingAuth, authUser, checkAuth } = useAuthStore();
-  useEffect(() => {
 
+  useEffect(() => {
     checkAuth();
-  }, [checkAuth])
+  }, [checkAuth]);
+
   if (isCheckingAuth && !authUser) {
     return (
-      <div className='flex items-center justify-center w-screen h-screen bg-[#121212]'>
-        <Loader className="size-8 animate-spin " />
+      <div className="flex items-center justify-center w-screen h-screen bg-[#121212]">
+        <Loader className="size-8 animate-spin" />
       </div>
-    )
+    );
   }
+
   return (
-    <Router>
+    <ThemeProvider defaultTheme="system" storageKey="vite-ui-theme">
+      <Layout>
+        <Routes>
+          <Route path="/" element={<AuthRedirect />} />
+          <Route path="/onboarding" element={<OnBoardingPage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/signup" element={<SignupPage />} />
 
-      <ThemeProvider defaultTheme="system" storageKey="vite-ui-theme">
-        <Layout>
-          <Routes>
-            <Route path="/" element={<AuthRedirect />} />
+          <Route
+            path="/chat"
+            element={
+              <ProtectedRoute>
+                <RedirectToLatestChat />
+              </ProtectedRoute>
+            }
+          />
 
-            <Route path="/onboarding" element={<OnBoardingPage />} />
+          <Route
+            path="/chat/:chatId"
+            element={
+              <ProtectedRoute>
+                <ChatPage />
+              </ProtectedRoute>
+            }
+          />
 
-            <Route path="/login" element={<LoginPage />}/>
-            <Route path="/signup" element={<SignupPage />}/>
+          <Route
+            path="/lawyer"
+            element={
+              <ProtectedRoute>
+                <LawyerPage />
+              </ProtectedRoute>
+            }
+          />
 
-            <Route
-              path="/chat"
-              element={
-                <ProtectedRoute>
-                  <RedirectToLatestChat />
-                </ProtectedRoute>
-              }
-            />
-
-            <Route
-              path="/chat/:chatId"
-              element={
-                <ProtectedRoute>
-                  <ChatPage />
-                </ProtectedRoute>
-              }
-            />
-            <Route
-              path="/lawyer/"
-              element={
-                <ProtectedRoute>
-                  <LawyerPage />
-                </ProtectedRoute>
-              }
-            />
-            <Route
-              path="/contact/"
-              element={
-                <ProtectedRoute>
-                  <ContactPage />
-                </ProtectedRoute>
-              }
-            />
-          </Routes>
-        </Layout>
-      </ThemeProvider>
-    </Router>
+          <Route
+            path="/contact"
+            element={
+              <ProtectedRoute>
+                <ContactPage />
+              </ProtectedRoute>
+            }
+          />
+        </Routes>
+      </Layout>
+    </ThemeProvider>
   );
 }
-
-export default App;
