4bf15ee6db50a93211cfbdcbfcbd76620c8c743d
SETHUKUMAR1709 <sethukumars774@gmail.com>
2025-12-20 21:22:13 +0530

    bug fix.

--- backend/routes/chatRoutes.js
+++ backend/routes/chatRoutes.js
@@ -11,6 +11,7 @@ import upload from "../lib/multer.js";
 const router = express.Router();
 
 router.get("/send", protect, sendMessage);
+router.post("/create", protect, createChat);
 router.post("/upload", protect, upload.fields([{ name: "files", maxCount: 3 }]), uploadChatFiles);
 router.post("/send", protect, sendMessage);
 router.get("/:chatId/messages", protect,getChatMessages);
